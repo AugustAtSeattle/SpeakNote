570c1158281495f0836fc5ff3851a1e0412bbe67
AugustAtSeattle <august.lin.usa@gmail.com>
2023-11-27 13:31:19 -0800

    Initial Commit

---
+++ SpeakNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpeakNote
+//
+//  Created by Sailor on 11/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
