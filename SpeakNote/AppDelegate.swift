30473ab2fe1e249ae229de63bae3421c8c9767b1
AugustAtSeattle <august.lin.usa@gmail.com>
2023-12-04 13:28:46 -0800

    Tune database #27

--- SpeakNote/AppDelegate.swift
+++ SpeakNote/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         _ = DatabaseManager.shared
-        testOPENAI()
+//        testOPENAI()
         return true
     }
     
